7abcd3fcc4f8d144017fabaa8a3a6d1244ebfeef
Evan-otero <Evan.otero19@gmail.com>
2019-08-22 00:01:29 +0000

    intital commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,27 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let parrot = ["A dog", "A cat", "A hamster", "My wife", "My brother"];
+  let table = [" ate", " kicked", " stole", " broke", " destroyed"];
+  let door = [
+    " my homework",
+    " my car",
+    " my backpack",
+    " my phone",
+    " my keys"
+  ];
+  let clock = [
+    " yesterday",
+    " on saturday",
+    " last week",
+    " this morning",
+    " last night"
+  ];
+  let generateexcuse =
+    parrot[Math.floor(Math.random() * parrot.length)] +
+    table[Math.floor(Math.random() * table.length)] +
+    door[Math.floor(Math.random() * door.length)] +
+    clock[Math.floor(Math.random() * clock.length)];
+
+  console.log(generateexcuse);
 };
